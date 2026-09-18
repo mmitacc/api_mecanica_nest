8d80ba3136715942ec230ca0fded123519d6a6a8
mmitacc <manuelmitacc@hotmail.com>
2026-09-18 16:15:02 -0500

    add-auth: usuario, auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,17 @@ import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { CreateUsuarioDto } from '../usuario/dto/create-usuario.dto.js';
 import { LoginAuthDto } from './dto/login-auth.dto.js';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard.js';
+import { RolesGuard } from '../auth/guards/roles.guard.js';
+import { Roles } from '../auth/decorators/roles.decorator.js';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('DUEÑO')
   @Post('register')
   async register(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.authService.register(createUsuarioDto);
--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,17 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsuarioService } from './usuario.service.js';
 import { CreateUsuarioDto } from './dto/create-usuario.dto.js';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto.js';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard.js';
+import { RolesGuard } from '../auth/guards/roles.guard.js';
+import { Roles } from '../auth/decorators/roles.decorator.js';
 
 @Controller('usuario')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('DUEÑO')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
-
+  
   @Post()
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuarioService.create(createUsuarioDto);
   }
-
+  
   @Get()
   findAll() {
     return this.usuarioService.findAll();
